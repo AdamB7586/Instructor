086b5f6b7f2f50daba19a8f7aa309b834bfa1278
Adam Binnersley <abinnersley@gmail.com>
2018-02-26 09:06:35 +0000

    Make child class compatible

--- src/Auto.php
+++ src/Auto.php
@@ -22,9 +22,10 @@ class Auto extends Instructor{
      * @param string $postcode This should be the postcode that you want to find the closest instructors to
      * @param int $limit The maximum number of instructors to display
      * @param boolean $cover If the search is only postcodes set this to true to only display instructors who have this listed as an area they cover
+     * @param boolean $hasOffer If you want to prioritise those with an offer first set this to true
      * @return array|false If any instructors cover the area will return an array of their details else will return false 
      */
-    public function findClosestInstructors($postcode, $limit = 50, $cover = true) {
+    public function findClosestInstructors($postcode, $limit = 50, $cover = true, $hasOffer = false) {
         $maps = new GoogleMapsGeocoder($postcode.', UK', 'xml');
         if($this->getAPIKey() !== false){$maps->setApiKey($this->getAPIKey());}
         $maps->geocode();
@@ -37,7 +38,7 @@ class Auto extends Instructor{
                 $coverSQL = "";
                 $distance = 15;
             }
-            return $this->listInstructors($this->db->query("SELECT *, (3959 * acos(cos(radians('{$maps->getLatitude()}')) * cos(radians(lat)) * cos(radians(lng) - radians('{$maps->getLongitude()}')) + sin(radians('{$maps->getLatitude()}')) * sin(radians(lat)))) AS `distance` FROM `{$this->instructor_table}` WHERE `active` = 1 AND `automatic` = 1{$coverSQL} HAVING `distance` < {$distance} ORDER BY `distance` LIMIT ".$limit.";"));
+            return $this->listInstructors($this->db->query("SELECT *, (3959 * acos(cos(radians('{$maps->getLatitude()}')) * cos(radians(lat)) * cos(radians(lng) - radians('{$maps->getLongitude()}')) + sin(radians('{$maps->getLatitude()}')) * sin(radians(lat)))) AS `distance` FROM `{$this->instructor_table}` WHERE `active` = 1 AND `automatic` = 1{$coverSQL} HAVING `distance` < {$distance} ORDER BY".($hasOffer !== false ? " `offer` DESC," : "")." `distance` LIMIT ".$limit.";"));
         }
         return false;
     }
